e4c933604ea02e344f3d4d3f057f88c0b4a82363
Rok Garbas <rok.garbas@gmail.com>
2008-10-26 16:24:12 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of collective.recipe.patch
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('collective', 'recipe', 'patch', 'README.txt')
+    + '\n' +
+    'Contributors\n' 
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' + 
+    read('CHANGES.txt')
+    + '\n' +
+   'Download\n'
+    '********\n'
+    )
+entry_point = 'collective.recipe.patch:Recipe'
+entry_points = {"zc.buildout": ["default = %s" % entry_point]}
+
+tests_require=['zope.testing', 'zc.buildout']
+
+setup(name='collective.recipe.patch',
+      version=version,
+      description="recipe for patching eggs",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='buildout recipe patch',
+      author='Rok Garbas',
+      author_email='rok.garbas@gmail.com',
+      url='http://github.com/garbas/collective.recipe.patch',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'zc.buildout'
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'collective.recipe.patch.tests.test_docs.test_suite',
+      entry_points=entry_points,
+      )
