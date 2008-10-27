ed8ff13a201950106372ea148591df6323eb471e
Rok Garbas <rok.garbas@gmail.com>
2008-10-27 17:30:04 +0100

    fixed egg_path

--- collective/recipe/patch/__init__.py
+++ collective/recipe/patch/__init__.py
@@ -14,38 +14,27 @@ class Recipe(object):
 
         self.options['python'] = self.buildout['buildout']['python']
         self.options['executable'] = self.buildout[self.options['python']]['executable']
-        self.options['eggs-directory'] = os.path.join(buildout['buildout']['directory'], 'eggs-patched')
-        self.options['develop-eggs-directory'] = os.path.join(buildout['buildout']['develop-eggs-directory'], 'develop-eggs-patched')
+        self.options['eggs-directory'] = buildout['buildout']['eggs-directory']
+        self.options['develop-eggs-directory'] = buildout['buildout']['develop-eggs-directory']
 
     def install(self):
         """Installer"""
-        print 'TESTING'
-        # install patched egg into patched directory
-        if self.buildout['buildout'].get('offline') == 'true':
-            ws = zc.buildout.easy_install.working_set(
-                [self.options['egg']], self.options['executable'],
-                [self.options['develop-eggs-directory'], self.options['eggs-directory']] )
-        else:
-            ws = zc.buildout.easy_install.install(
-                [self.options['egg']], self.options['eggs-directory'],
-                links           = self.buildout['buildout'].get('find-links', ''),
-                index           = self.buildout['buildout'].get('index', ''),
-                executable      = self.options['executable'],
-                path            = [self.options['develop-eggs-directory']],
-                newest          = self.buildout['buildout'].get('newest') == 'true',
-                allow_hosts     = self.buildout['buildout'].get('allow-hosts', '*'),
-                always_unzip    = 'true', )
+        
+        ws = zc.buildout.easy_install.install(
+            [self.options['egg']], self.options['eggs-directory'],
+            links           = self.buildout['buildout'].get('find-links', ''),
+            index           = self.buildout['buildout'].get('index', ''),
+            executable      = self.options['executable'],
+            path            = [self.options['develop-eggs-directory']],
+            newest          = self.buildout['buildout'].get('newest') == 'true',
+            allow_hosts     = self.buildout['buildout'].get('allow-hosts', '*'),
+            always_unzip    = 'true', )
 
-
-        egg_path = ws.by_key.values()[0].location
+        egg_path = ws.require(self.options['egg'])[0].location
         patch_path = os.path.abspath(self.options['patch'])
-        print open(egg_path+'/demo.py').read()
         os.chdir(egg_path)
         os.system('patch -p0 < %s' % patch_path)
-        # Return files that were created by the recipe. The buildout
-        # will remove all returned files upon reinstall.
-        print 'PATCHED'
-        print open(egg_path+'/demo.py').read()
+
         return egg_path,
 
     def update(self):
