c1fe4b426913f74ae08a219f33036ded8eb94d5e
Rok Garbas <rok.garbas@gmail.com>
2008-10-27 12:33:33 +0100

    intial code added

--- collective/recipe/patch/__init__.py
+++ collective/recipe/patch/__init__.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
-"""Recipe patch"""
+"""Recipe for applying patches"""
+
+import os
+import zc.recipe.egg
+
 
 class Recipe(object):
     """zc.buildout recipe"""
 
     def __init__(self, buildout, name, options):
+        self.egg = zc.recipe.egg.Scripts(buildout, name, options)
         self.buildout, self.name, self.options = buildout, name, options
 
     def install(self):
         """Installer"""
-        # XXX Implement recipe functionality here
-        
+
+        # 
+        egg_path = ??? self.options['egg']
+
+        # 
+        patch_content = open(os.path.join(curdir, PATCH_NAME)).read()
+        copy = os.path.join(ZODB_FOLDER, PATCH_NAME)
+        f = open(copy, 'w')
+        try:
+            f.write(patch_content)
+        finally:
+            f.close()
+
+        os.chdir(egg_path)
+        os.system('patch -p1 < %s' % patch_path)
+
         # Return files that were created by the recipe. The buildout
         # will remove all returned files upon reinstall.
         return tuple()
